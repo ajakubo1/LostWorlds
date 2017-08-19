205acee5b89253c0f6ca2cf2f80ff80ac83f928a
Adam Jakubowski <ajakubo1@gmail.com>
2017-08-19 22:48:32 +0200

    Making the loop actually do something

--- game/core/loop.js
+++ game/core/loop.js
@@ -2,20 +2,39 @@ export default class GameLoop {
   constructor(ops) {
     this.timeUPS = 1000.0 / ops;
     this.running = false;
+    this.lastUpdateTime = undefined;
+    this._render = this._render.bind(this);
+    this._update = this._update.bind(this);
   }
 
   _render() {
-    console.info('_render')
+    //TODO here I should run all of the render stuff
+
+    if (this.running) {
+      window.requestAnimationFrame(this._render);
+    }
   }
 
   _update() {
-    console.info('_update')
+    let lastUpdateTimedelta = window.performance.now() - this.lastUpdateTime;
+    while (lastUpdateTimedelta > this.timeUPS) {
+      //TODO - here I should call update function
+      this.lastUpdateTime += this.timeUPS;
+      lastUpdateTimedelta -= this.timeUPS;
+    }
+
+    if (this.running) {
+      setTimeout(this._update, 0);
+    }
   }
 
   start() {
     this.running = true;
+
     window.requestAnimationFrame(this._render);
-    setTimeout(this._update, 0);
+
+    this.lastUpdateTime = window.performance.now();
+    setTimeout(this._update, this.timeUPS);
   }
 
   stop() {
