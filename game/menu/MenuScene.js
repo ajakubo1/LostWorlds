e9e6375680ed1eba8df896b0452b73b523c7a6bc
Adam Jakubowski <ajakubo1@gmail.com>
2017-08-30 23:56:48 +0200

    Making dialog box simpler

--- game/menu/MenuScene.js
+++ game/menu/MenuScene.js
@@ -11,18 +11,13 @@ export default class MenuScene extends Scene {
     super();
 
     this.onSkip = this.onSkip.bind(this);
-    this.textFinished = this.textFinished.bind(this);
-    this.continueClicked = this.continueClicked.bind(this);
-    this.onYes = this.onYes.bind(this);
-    this.onNo = this.onNo.bind(this);
+
     this.restartLevel = this.restartLevel.bind(this);
+    this.moveToLevelChoice = this.moveToLevelChoice.bind(this);
 
     this.background = new Background(0, 0, Engine.width, Engine.height);
 
     this.scientist = new Scientist(Engine.width / 2 - 200, Engine.height / 2 - 75, 100, 150);
-    this.dialog = new Dialog(Engine.width / 2, Engine.height / 2 - 75, 300, 100);
-    this.dialog.setPixelSize(3);
-    this.dialog.setCallback(this.textFinished);
 
     this.texts = [
       "Oh, what a mess!",           // 0
@@ -34,98 +29,29 @@ export default class MenuScene extends Scene {
       "Oh, you there!",             // 6
       "Yes you!",                   // 7
       "Are you the new intern?",    // 8
-      "You are not?",               // 9
-      "What are you doing here?",   // 10
-      "Security! Security!",        // 11
       "Splendid, my dear lad!",     // 12
       "You will help me out!",      // 13
-      "...",
-      "Everything is lost!",
-      "Every label displaced!",
-      "Every blackbox unmarked",
-      "Every battery taken out!",
-      "Good thing I marked all of the blackboxes",
-      "Or we wouldn't even know what's inside",
-      "OK then.",
-      "Let's get down to business",
-      ""
+      "...",                        // 14
+      "Everything bit of data is lost!", // 15
+      "Every label displaced!", // 16
+      "Every battery taken out!", // 17
+      "Good thing I marked all of the blackboxes", // 18
+      "Or we wouldn't even know what's inside", // 19
+      "OK then. Blackboxes... Let me explain.", // 20
     ];
 
-    this.decisionTree = [
-      [8, 12, 9]
-    ];
-
-    this.currentDecision = 0;
-
-    this.end = [
-      11
-    ];
-
-    this.finished = 20;
-
-    this.currentText = 0;
-
-    this.dialog.textToLoad(this.texts[this.currentText]);
-
-    this.yes = new Button(Engine.width / 2, Engine.height / 2 + 20, 50, 20,
-      'yes', undefined, undefined, this.onYes);
-
-    this.no = new Button(Engine.width / 2 + 250, Engine.height / 2 + 20, 50, 20,
-      'no', undefined, undefined, this.onNo);
-
-    this.continue = new Button(Engine.width / 2 + 250, Engine.height / 2 + 20, 50, 20,
-      '[...]', undefined, undefined, this.continueClicked);
+    this.dialog = new Dialog(
+      Engine.width / 2, Engine.height / 2 - 75, 300, 100, this.texts
+    );
+    this.dialog.setPixelSize(3);
+    this.dialog.setCallback(this.moveToLevelChoice);
 
     this.skip = new Button(Engine.width - 125, 25, 100, 30,
       'skip & play', undefined, undefined, this.onSkip);
 
     this.dialogEnabled = false;
-    this.choiceEnabled = false;
-    this.continueEnabled = false;
 
     this.ticksPassed = 0;
-  }
-
-  textFinished() {
-    if (this.currentText === this.finished) {
-      this.continue.text = 'play';
-    }
-
-    if(this.end.indexOf(this.currentText) !== -1) {
-      this.skip.text = 'restart';
-      this.skip.onClick = this.restartLevel;
-      return;
-    }
-
-    if (this.currentText === this.decisionTree[this.currentDecision][0]) {
-      this.choiceEnabled = true;
-      return;
-    }
-
-    this.continueEnabled = true;
-  }
-
-  onYes() {
-    this.choiceEnabled = false;
-    this.currentText = this.decisionTree[this.currentDecision][1];
-    this.dialog.textToLoad(this.texts[this.currentText]);
-  }
-
-  onNo() {
-    this.choiceEnabled = false;
-    this.currentText = this.decisionTree[this.currentDecision][2];
-    this.dialog.textToLoad(this.texts[this.currentText]);
-  }
-
-  continueClicked() {
-    this.continueEnabled = false;
-
-    if(this.currentText === this.finished) {
-      this.moveToLevelChoice();
-    } else {
-      this.currentText += 1;
-      this.dialog.textToLoad(this.texts[this.currentText]);
-    }
   }
 
   onSkip() {
@@ -145,24 +71,12 @@ export default class MenuScene extends Scene {
   }
 
   moved(x, y) {
-    if (this.choiceEnabled && this.yes.state === 0 && this.yes.inRange(x, y)) {
-      this.yes.setHover();
-    } else if (this.choiceEnabled && this.yes.state === 0 && this.no.inRange(x, y)) {
-      this.no.setHover();
-    } else if (this.continueEnabled && this.continue.state === 0 && this.continue.inRange(x, y)) {
-      this.continue.setHover();
-    } else if (this.skip.state === 0 && this.skip.inRange(x, y)) {
+    if (this.skip.state === 0 && this.skip.inRange(x, y)) {
       this.skip.setHover();
-    }
-
-    if (this.choiceEnabled && this.yes.state === 1 && !this.yes.inRange(x, y)) {
-      this.yes.setNormal();
-    } else if (this.choiceEnabled && this.no.state === 1 && !this.no.inRange(x, y)) {
-      this.no.setNormal();
-    } else if (this.continueEnabled && this.continue.state === 1 && !this.continue.inRange(x, y)) {
-      this.continue.setNormal();
     } else if (this.skip.state === 1 && !this.skip.inRange(x, y)) {
       this.skip.setNormal();
+    } else {
+      this.dialog.moved(x, y)
     }
   }
 
@@ -171,17 +85,12 @@ export default class MenuScene extends Scene {
   }
 
   pressed(x, y) {
-    if (this.choiceEnabled && this.yes.inRange(x, y)) {
-      this.yes.click();
-    } else if (this.choiceEnabled && this.no.inRange(x, y)) {
-      this.no.click();
-    } else if (this.continue.inRange(x, y)) {
-      this.continue.click();
+    if (this.dialog.inRange(x, y)) {
+      this.dialog.pressed(x, y);
     } else if (this.skip.inRange(x, y)) {
       this.skip.click();
     } else if (this.dialogEnabled) {
       this.dialog.loadText();
-      this.textFinished();
     }
   }
 
@@ -191,7 +100,6 @@ export default class MenuScene extends Scene {
     if (this.ticksPassed === 100 && !this.dialogEnabled) {
       this.dialogEnabled = true;
     }
-
 
     if (this.dialogEnabled) {
       this.dialog.update();
@@ -205,16 +113,6 @@ export default class MenuScene extends Scene {
     if (this.dialogEnabled) {
       this.dialog.render(context);
     }
-
-    if (this.continueEnabled) {
-      this.continue.render(context);
-    }
-
-    if (this.choiceEnabled) {
-      this.yes.render(context);
-      this.no.render(context);
-    }
-
     this.skip.render(context);
   }
 }
--- game/menu/objects/Dialog.js
+++ game/menu/objects/Dialog.js
@@ -2,8 +2,28 @@ import Renderable from '../../core/Renderable';
 import Engine from '../../core/Engine';
 import { IDENTIFIERS as ASSET_IDENTIFIERS } from '../../core/Assets';
 import {fill} from "../../core/Letters";
+import Button from "../../core/Button";
 
 export default class Dialog extends Renderable {
+
+  constructor(x, y, width, height, texts) {
+    super(x, y, width, height);
+    this.continueClicked = this.continueClicked.bind(this);
+
+    this.continue = new Button(x + width - 50, y + height - 20, 50, 20,
+      '[...]', undefined, undefined, this.continueClicked);
+    
+    this.finished = texts.length - 1;
+
+    this.currentText = 0;
+
+    this.texts = texts;
+
+    this.continueEnabled = false;
+
+    this.textToLoad(this.texts[this.currentText]);
+  }
+
   getImage() {
     return Engine.getAsset(ASSET_IDENTIFIERS.PLANET_SQUARE)
   }
@@ -25,6 +45,38 @@ export default class Dialog extends Renderable {
     this.callback = callback;
   }
 
+  showContinue() {
+    this.stop = true;
+    this.continueEnabled = true;
+  }
+
+  continueClicked() {
+    this.continueEnabled = false;
+
+    if(this.currentText === this.finished) {
+      this.callback();
+    } else {
+      this.currentText += 1;
+      this.textToLoad(this.texts[this.currentText]);
+    }
+  }
+
+  pressed(x, y) {
+    if(this.continueEnabled && this.continue.inRange(x, y)) {
+      this.continue.click();
+    } else {
+      this.loadText();
+    }
+  }
+
+  moved(x, y) {
+    if(this.continueEnabled && this.continue.state === 0 && this.continue.inRange(x, y)) {
+      this.continue.setHover();
+    } else if(this.continueEnabled && this.continue.state === 1 && !this.continue.inRange(x, y)) {
+      this.continue.setNormal();
+    }
+  }
+
   update() {
     this.step += 1;
     if (!this.stop && this.step % this.interval === 0) {
@@ -32,19 +84,22 @@ export default class Dialog extends Renderable {
       this.currentLetter += 1;
 
       if (this.currentLetter === this.text.length) {
-        this.stop = true;
-        this.callback();
+        this.showContinue();
       }
     }
   }
 
   loadText() {
     this.textSoFar = this.text;
-    this.stop = true;
+    this.showContinue();
   }
 
   render(context) {
     context.drawImage(this.image, this.x, this.y, this.width, this.height);
     fill(this.x, this.y, this.width, this.height, this.textSoFar, context, 'red', this.size);
+
+    if(this.continueEnabled) {
+      this.continue.render(context);
+    }
   }
 }
