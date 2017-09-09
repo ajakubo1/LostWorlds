bec2900ae3aedf8661d7f559975fcb88447a6eba
Adam Jakubowski <ajakubo1@gmail.com>
2017-09-09 09:40:13 +0200

    going mobile

--- game/core/Events.js
+++ game/core/Events.js
@@ -4,21 +4,30 @@ export default class Events {
   constructor() {
     this.onMouseDown = this.onMouseDown.bind(this);
     this.onMouseMove = this.onMouseMove.bind(this);
-    this.onMouseLeave = this.onMouseLeave.bind(this);
-    this.onMouseUp = this.onMouseUp.bind(this);
+    this.onMouseUpLeave = this.onMouseUpLeave.bind(this);
+    this.onTouchStart = this.onTouchStart.bind(this);
+    this.onTouchEndCancel = this.onTouchEndCancel.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.isMobile = false;
   }
 
 
   addOnPressEvent (element, callback) {
     this.finished = callback;
     this.element = element;
-    this.element.addEventListener('mousedown', this.onMouseDown);
-    this.element.addEventListener('mousemove', this.onMouseMove);
+    if (!this.isMobile) {
+      this.element.addEventListener('mousedown', this.onMouseDown);
+      this.element.addEventListener('mousemove', this.onMouseMove);
+    }
+    this.element.addEventListener('touchstart', this.onTouchStart);
   }
 
   removeOnPressEvent () {
-    this.element.removeEventListener('mousedown', this.onMouseDown);
-    this.element.removeEventListener('mousemove', this.onMouseMove);
+    if (!this.isMobile) {
+      this.element.removeEventListener('mousedown', this.onMouseDown);
+      this.element.removeEventListener('mousemove', this.onMouseMove);
+    }
+    this.element.removeEventListener('touchstart', this.onTouchStart);
     this.finished = null;
     this.element = null;
   }
@@ -39,8 +48,8 @@ export default class Events {
     const x = (event.clientX / event.target.offsetWidth) * Engine.width;
     const y = (event.clientY / event.target.offsetHeight) * Engine.height;
 
-    this.element.addEventListener('mouseup', this.onMouseUp);
-    this.element.addEventListener('mouseleave', this.onMouseLeave);
+    this.element.addEventListener('mouseup', this.onMouseUpLeave);
+    this.element.addEventListener('mouseleave', this.onMouseUpLeave);
     this.callbackPressed(x, y);
   }
 
@@ -50,15 +59,40 @@ export default class Events {
     this.callbackMoved(x, y);
   }
 
-  onMouseUp(event) {
-    this.element.removeEventListener('mouseup', this.onMouseUp);
-    this.element.removeEventListener('mouseleave', this.onMouseLeave);
+  onMouseUpLeave(event) {
+    this.element.removeEventListener('mouseup', this.onMouseUpLeave);
+    this.element.removeEventListener('mouseleave', this.onMouseUpLeave);
     this.callbackReleased();
   }
 
-  onMouseLeave(event) {
-    this.element.removeEventListener('mouseup', this.onMouseUp);
-    this.element.removeEventListener('mouseleave', this.onMouseLeave);
+  onTouchStart(event) {
+    if (!this.isMobile) {
+      this.isMobile = true;
+      this.element.removeEventListener('mousedown', this.onMouseDown);
+      this.element.removeEventListener('mousemove', this.onMouseMove);
+      this.element.removeEventListener('mouseup', this.onMouseUpLeave);
+      this.element.removeEventListener('mouseleave', this.onMouseUpLeave);
+    }
+
+    const x = (event.touches[0].clientX / event.target.offsetWidth) * Engine.width;
+    const y = (event.touches[0].clientY / event.target.offsetHeight) * Engine.height;
+
+    this.element.addEventListener('touchmove', this.onTouchMove);
+    this.element.addEventListener('touchend', this.onTouchEndCancel);
+    this.element.addEventListener('touchcancel', this.onTouchEndCancel);
+    this.callbackPressed(x, y);
+  }
+
+  onTouchEndCancel(event) {
+    this.element.removeEventListener('touchmove', this.onTouchMove);
+    this.element.removeEventListener('touchend', this.onTouchEndCancel);
+    this.element.removeEventListener('touchcancel', this.onTouchEndCancel);
     this.callbackReleased();
+  }
+
+  onTouchMove(event) {
+    const x = (event.touches[0].clientX / event.target.offsetWidth) * Engine.width;
+    const y = (event.touches[0].clientY / event.target.offsetHeight) * Engine.height;
+    this.callbackMoved(x, y);
   }
 }
